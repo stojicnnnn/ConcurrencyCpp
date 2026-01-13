9ccf082be14000150ed185a2998a2b4d9f3f62b6
Nikola Stojic <snikola.stojic@gmail.com>
2026-01-13 13:21:38 +0100

    Remove build artifacts from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nikola/code/ConcurrencyCpp
-# Build directory: /home/nikola/code/ConcurrencyCpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("src")
